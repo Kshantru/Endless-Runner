3e51af1d789b524946829f7c285a334857a6d8aa
Kshantru <64325322+Kshantru@users.noreply.github.com>
2020-10-15 19:07:28 +0530

    Added obstacle object in TileManager

--- runner/scripts/TileManager.cs
+++ runner/scripts/TileManager.cs
@@ -12,6 +12,8 @@ public class TileManager : MonoBehaviour
     private int amnTilesOnScreen = 7;
     private int lastPrefabIndex = 0;
 
+    public GameObject obstaclePrefab;
+
     private List<GameObject> activeTiles;
 
     // Start is called before the first frame update
@@ -30,6 +32,7 @@ public class TileManager : MonoBehaviour
                 SpawnTile();
             }
         }
+        SpawnObstacle();
     }
 
     // Update is called once per frame
@@ -80,5 +83,15 @@ public class TileManager : MonoBehaviour
         }
         lastPrefabIndex = randomIndex;
         return randomIndex;
+    }
+
+    public void SpawnObstacle()
+    {
+        // Choose a random point to spawn the obstacle
+        int obstacleSpawnIndex = Random.Range(2, 5);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+
+        // Spawn the obstace at the position
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
     }
 }
