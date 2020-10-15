552461e0d1a0952f8cfd41d00f137527cdbfaf7e
Kshantru <64325322+Kshantru@users.noreply.github.com>
2020-10-15 20:25:00 +0530

    Update ObstacleManager.cs

--- runner/scripts/ObstacleManager.cs
+++ runner/scripts/ObstacleManager.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class ObstacleManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     // private Transform playerTransform;
-    // private float spawnZ = 20.0f;
-    // private float tileLength = 20.0f;
+    private float spawnZ = 20.0f;
+     private float spawnGap = 6.0f;
     //private float safeZone = 32.0f;
     // private int amnTilesOnScreen = 7;
-    //private int lastPrefabIndex = 0;
+    private int lastPrefabIndex = 0;
 
-    //private List<GameObject> activeObstacles;
+    private List<GameObject> activeObstacles;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnObstacle();
+        SpawnObstacle2();
         //activeObstacles = new List<GameObject>();
     }
 		// Update is called once per frame
@@ -36,4 +37,37 @@ public class ObstacleManager : MonoBehaviour
 			// Spawn the obstace at the position
 			Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
 		}
-	}
+
+    public void SpawnObstacle2(int prefabIndex = -1)
+    {
+        GameObject go;
+        if (prefabIndex == -1)
+        {
+            go = Instantiate(obstaclePrefabs[RandomPrefabIndex()]) as GameObject;
+        }
+        else
+        {
+            go = Instantiate(obstaclePrefabs[prefabIndex]) as GameObject;
+        }
+
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += spawnGap;
+        activeObstacles.Add(go);
+
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (obstaclePrefabs.Length <= 1)
+            return 0;
+
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, obstaclePrefabs.Length);
+        }
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+    }
+}
